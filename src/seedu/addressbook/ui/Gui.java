34d7079065f9ff62e4af76676a11997bdd450e75
Ying Ying Choi <yingying_choi@hotmail.com>
2016-09-15 13:17:12 +0800

    Added an icon

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -2,6 +2,7 @@ package seedu.addressbook.ui;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.Main;
@@ -39,6 +40,7 @@ public class Gui {
         loader.setLocation(Main.class.getResource("ui" + File.separator + "mainwindow.fxml"));
         stage.setTitle(version);
         stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
+        stage.getIcons().add(new Image("file:src/seedu/addressbook/ui/icon.png"));
         stage.show();
         MainWindow mainWindow = loader.getController();
         mainWindow.setLogic(logic);
